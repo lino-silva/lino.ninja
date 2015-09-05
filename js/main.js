075df63d92b413746412c0ea67bd212c40158786
Lino Silva <okulto@gmail.com>
2015-09-05 18:40:28 +0100

    Added styling

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
 var timelineBlocks = $('.timeline-block'),
-  offset = 0.8;
+  offset = 0.8,
+  jQWindow = $(window),
+  windowHeight = jQWindow.height(),
+  profile_image = document.getElementById('profile_image');
 
 //hide timeline blocks which are outside the viewport
 hideBlocks(timelineBlocks, offset);
 
 //on scolling, show/animate timeline blocks when enter the viewport
-$(window).on('scroll', function(){
+jQWindow.on('scroll', function(){
   (!window.requestAnimationFrame) 
-    ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-    : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+    ? setTimeout(function () { 
+        showBlocks(timelineBlocks, offset);
+
+      }, 100)
+    : window.requestAnimationFrame(function () { 
+      showBlocks(timelineBlocks, offset); 
+    });
+}).on('resize', function () {
+  windowHeight = jQWindow.height();
 });
 
 function hideBlocks(blocks, offset) {
-  blocks.each(function(){
-    ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.timeline-img, .timeline-content').addClass('is-hidden');
+  var scrollTop = jQWindow.scrollTop();
+  blocks.each(function () {
+    var jQThis = $(this);
+    ( jQThis.offset().top > scrollTop + windowHeight * offset ) && jQThis.find('.timeline-img, .timeline-content').addClass('is-hidden');
   });
+
+  (scrollTop > 150)?
+    profile_image.classList.add('collapsed'):
+    profile_image.classList.remove('collapsed');
 }
 
 function showBlocks(blocks, offset) {
-  blocks.each(function(){
-    ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.timeline-img').hasClass('is-hidden') ) && $(this).find('.timeline-img, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
+  var scrollTop = jQWindow.scrollTop();
+  blocks.each(function () {
+    var jQThis = $(this);
+    (jQThis.offset().top <= scrollTop + windowHeight * offset && jQThis.find('.timeline-img').hasClass('is-hidden') ) && jQThis.find('.timeline-img, .timeline-content').removeClass('is-hidden').addClass('bounce-in');
   });
+  
+  (scrollTop > 150)?
+    profile_image.classList.add('collapsed'):
+    profile_image.classList.remove('collapsed');
 }
 
 var story_container = $('.story-container');
